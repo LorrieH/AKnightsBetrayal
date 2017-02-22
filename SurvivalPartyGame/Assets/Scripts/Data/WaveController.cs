96583fa0428b94ef5c13c993f80f9f7b15c3737c
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-22 15:56:55 +0100

    push

---
+++ SurvivalPartyGame/Assets/Scripts/Data/WaveController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    EnemySpawner _enemySpawner;
+
+    private int _enemiesToSpawn = 16;
+    private int _maxEnemies = 15;
+    private int _enemiesSpawned;
+    private float timer;
+	
+    void Start()
+    {
+        _enemySpawner = GameObject.FindWithTag("EnemySpawner").GetComponent<EnemySpawner>();
+    }
+
+	void Update () {
+        if (_enemiesSpawned < _enemiesToSpawn)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.5)
+            {
+                NextWave();
+            }
+        }
+	}
+
+    void NextWave()
+    {
+        if(_enemySpawner.spawnedEnemies.Count < _maxEnemies)
+        {
+            _enemySpawner.SpawnEnemy();
+            _enemiesSpawned++;
+        }
+        timer = 0;
+    }
+}
--- SurvivalPartyGame/Assets/Scripts/Enemy/EnemySpawner.cs
+++ SurvivalPartyGame/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -7,6 +7,8 @@ public class EnemySpawner : EnemyDatabase {
 
     [SerializeField]private List<GameObject> _enemyTypes = new List<GameObject>();
     [SerializeField]private Transform _enemyParentObject;
+
+    [HideInInspector]public List<GameObject> spawnedEnemies = new List<GameObject>();
     private List<Transform> _spawnpoints = new List<Transform>();
 
     private int _enemyTypeCount;
@@ -22,7 +24,8 @@ public class EnemySpawner : EnemyDatabase {
     public void SpawnEnemy()
     {
         int randomEnemy = Random.Range(0, _enemyTypeCount);
-        GameObject spawnedEnemy = Instantiate(_enemyTypes[0]);
+        GameObject spawnedEnemy = Instantiate(_enemyTypes[randomEnemy]);
+        spawnedEnemies.Add(spawnedEnemy);
         spawnedEnemy.transform.position = _spawnpoints[Random.Range(0, _spawnpoints.Count)].position;
         spawnedEnemy.transform.SetParent(_enemyParentObject);
     }
