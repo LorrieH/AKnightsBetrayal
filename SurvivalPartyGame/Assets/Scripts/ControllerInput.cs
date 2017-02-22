9d73c9e7e49f5c6c84453d0d7164f8babb1cc1db
Jprummel <jordiprummel@live.nl>
2017-02-22 09:41:48 +0100

    Input script
    Input Delegates

---
+++ SurvivalPartyGame/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+
+    public delegate void InputDelegate();
+    public delegate void MovementDelegate();
+    public static event MovementDelegate MovementInput;
+    public static event InputDelegate AttackInput;
+
+    void Update()
+    {
+        if (Input.GetButtonDown(InputAxes.XBOX_X) && AttackInput != null)
+        {
+            AttackInput();
+        }
+
+        if (MovementInput != null)
+        {
+            MovementInput();
+        }
+    }
+}
