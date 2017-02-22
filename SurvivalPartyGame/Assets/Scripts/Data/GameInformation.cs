2898cdf4dabd83df3d2e06736bb12251b40ea5ab
Jprummel <jordiprummel@live.nl>
2017-02-22 14:11:07 +0100

    GameInformation

---
+++ SurvivalPartyGame/Assets/Scripts/Data/GameInformation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInformation : MonoBehaviour {
+
+    public static int HighestWave;
+    public static int Wave;
+}
