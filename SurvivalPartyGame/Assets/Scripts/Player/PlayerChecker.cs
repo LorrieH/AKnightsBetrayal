f0b8dceeec45ab79683c4183ec70b2949d0aaaef
Jprummel <jordiprummel@live.nl>
2017-02-23 12:59:59 +0100

    Playerchecker start

---
+++ SurvivalPartyGame/Assets/Scripts/Player/PlayerChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChecker : MonoBehaviour {
+
+    [SerializeField]private List<GameObject> _players = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        AddPlayers();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddPlayers()
+    {
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag(Tags.PLAYER))
+        {
+            _players.Add(player);
+        }
+    }
+}
